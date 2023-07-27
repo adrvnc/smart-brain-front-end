a91cd45cdff97fed1a1a985ede6ff3d0b45b2b38
Adrian Carter <adriancodesgit@gmail.com>
2023-07-26 21:10:15 -0500

    changed server url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // The purpose of using config.js is to keep the server URL and other configurations in one place and make it easier to modify them without modifying the client-side code directly.
 // By centralizing the server URL and other configurations in the config.js file, you can easily modify the URL in one place, and it will automatically reflect in all components that import and use the config.js file.
 const config = {
-    serverUrl: 'https://smart-brain-node-server.onrender.com'
+    serverUrl: 'http://localhost:3001'
   };
   
 export default config;
